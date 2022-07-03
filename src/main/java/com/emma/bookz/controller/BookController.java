cc2c9a7ccf4c369ffeae39294f71f0a408cfffcf
Emma Maikuri <maikuriemma@gmail.com>
2022-07-03 13:29:43 +0300

    Added thymelyf

--- src/main/java/com/emma/bookz/controller/BookController.java
+++ src/main/java/com/emma/bookz/controller/BookController.java
@@ -2,6 +2,8 @@ package com.emma.bookz.controller;
 
 import com.emma.bookz.service.BookService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BookController {
@@ -10,6 +12,11 @@ public class BookController {
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
-    //hamdler method to handle list of books and return model and view
-    public String listBooks
+    //handler method to handle list of books and return model and view
+    @GetMapping("/books")
+    public String listBooks(Model model){
+        model.addAttribute("book", bookService.getAllBooks());
+                return "books";
+
+    }
 }
